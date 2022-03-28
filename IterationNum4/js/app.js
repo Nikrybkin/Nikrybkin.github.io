08c950740cff9dfb40dbae15513ef8266fbf20dd
Nikrybkin <71150246+Nikrybkin@users.noreply.github.com>
2022-03-28 22:16:41 +0300

    Add files via upload

---
+++ IterationNum4/js/app.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x474A51,
+    shininess: 0.00,
+    waveHeight: 40.00,
+    waveSpeed: 0.05,
+    zoom: 0.97
+  })
